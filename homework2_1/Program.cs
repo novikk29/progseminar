f842114e33f8b825da311346f25cfeb2aae37f11
Andrei <novikk1986@ya.ru>
2022-12-23 18:03:21 +0300

    add two task

---
+++ homework2_1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное
+ число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число:");
+int number = int.Parse(Console.ReadLine()!);
+int ExtractionSecondDigit(int number)
+{
+    int num2 = number / 10;
+    int result = num2 % 10;
+    return result;
+}
+int SecondDigit = ExtractionSecondDigit(number);
+
+Console.WriteLine($"вторая цифра числа {number}: {SecondDigit}"); 
---
+++ homework2_2/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 
+Задача 15: Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет,
+является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/ 
+
+Console.Clear();
+Console.WriteLine("Введите цифру: ");
+int result = int.Parse(Console.ReadLine()!);
+if (result == 6 || result == 7)
+{
+    Console.WriteLine("выходной день");
+}
+else if (result > 7 || result < 1)
+{
+    Console.WriteLine("это не день недели");
+}
+else Console.WriteLine("будний день");
