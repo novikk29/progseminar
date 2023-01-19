1690148c45aad2fd1021bed10726a70743e8dfc8
Andrei <novikk1986@ya.ru>
2023-01-19 18:05:51 +0300

    add homework4

---
+++ homework4_1/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/ 
+
+Console.Clear();
+int Res(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+Console.WriteLine("введите A: ");
+int numA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите B: ");
+int numB = int.Parse(Console.ReadLine()!);
+
+int res = Res(numA, numB);
+Console.WriteLine($"число {numA} в степени {numB} равно {res}");
+
---
+++ homework4_3/Program.cs
@@ -0,0 +1,33 @@
+﻿/* 
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/ 
+Console.Clear();
+int[] GetArrayRandom(int length)
+{
+    int[] arr = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(0, 10);
+    }
+    return arr;
+}
+void ShowArray(int[] array)
+{
+    Console.Write($"[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length -1)
+        {
+            Console.Write($"{array[i]},");
+        }
+        else 
+        {
+            Console.Write($"{array[i]}]");
+        }
+    }
+}
+int[] array = GetArrayRandom(8);
+ShowArray(array);
