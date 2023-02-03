38cd93afc6016879eb4c6a2b4d852cfe4527b067
Andrei <novikk1986@ya.ru>
2023-02-03 15:04:04 +0300

    add homework9

---
+++ homework9_1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+
+Console.Clear();
+int getIntFromUser(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+string getStrRange(int start, int finish)
+{
+    if(start == finish)
+    {
+        return start + "";
+    }
+    return getStrRange(start + 1, finish) + "," + start;
+}
+int userNumberFinish = getIntFromUser("введите N");
+string range = getStrRange(1, userNumberFinish);
+Console.WriteLine(range);
---
+++ homework9_2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке
+от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Clear();
+int getIntFromUser(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int sumNaturalNumber(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + sumNaturalNumber(m, n - 1);
+}
+
+int m = getIntFromUser("введите M");
+int n = getIntFromUser("введите N");
+Console.WriteLine("--------------------------------------------------------------");
+Console.WriteLine($"В промежутке от {m} до {n}, сумма натуральных элементов равна {sumNaturalNumber(m, n)}");
+Console.WriteLine("--------------------------------------------------------------");
+
